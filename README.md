a7bb063d0c6bf83dbd377a3587f718216d19678c
jbooth1@us.ibm.com <jbooth1@us.ibm.com>
2016-12-14 16:03:21 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# sample-article-create
+This is a sample of a custom web application for creating new "Article" content items with images in IBM Watson Content Hub (WCH).
+
+This sample shows:
+* Authenticating to the Watson Content Hub and calling APIs that require authentication.
+* Using the authoring services for resources, assets, and content to upload a resource, create an asset, and create an "Article" content item that includes the image. 
+
+This sample can be used to create new articles that will be displayed in this earlier sample: https://github.com/ibm-wch/sample-article-carousel
+
+This screenshot shows the completed form for a new Article:
+![Alt text](/docs/create-article-screenshot.jpg?raw=true "Sample screenshot")
+
+###Running the sample
+
+#### 1. Download the files
+
+Download the application files (html, js, and css) from the 'public' folder into any folder on your workstation.
+
+#### 2. Update the user credentials
+
+This sample uses hard-coded user name and password set in the app.js file. Update the name and password values in that file.
+
+To avoid putting credentials in the source you could change the application to provide browser inputs for username and password.
+
+####3. Create the "Article" content type
+
+This application requires that you have an "Article" content type available. This is the same content type used in these other samples:
+- https://github.com/ibm-wch/sample-article-angular
+- https://github.com/ibm-wch/sample-article-carousel
+
+To create the Article content type, in the WCH user interface, under Content Model, Types, create an Article content type with the following elements. To set the "custom display" values, click on the little gear icon for the element.
+
+|Element name | Element type | Element custom display settings |
+| --- | --- | --- |
+|Title | Text | (none) |
+| Summary | Text | (none) |
+| Author | Text | (none) |
+| Body | Text | Multi line, Field width 100, Field height 10 |
+| Image | Image | (none) |
+| Publish Date | Date (Field type "Single date") | (none) |
+
+#### 4. Enable CORS support for your tenant
+
+For this scenario you will need to enable CORS support for your tenant. To control the CORS enablement for Watson Content Hub, go to Hub set up -> General settings -> Security tab. After adding your domain (or "*" for any domain), be sure to click the Save button at the top right of the screen.
+
+#### 5. Load index.html in a browser
+
+You can do this right from the file system in Firefox, Chrome, or Safari browsers. Alternatively you can make the files available on any web server and open index.html in a browser using your web server URL.
+
+###Implementation notes
+
+####Specifying the content type when creating a content item
+
+The API for creating a new content items requires the ID of a content type rather than the name of the content type. In this sample the ID of the "article" type is determined by doing a search for a content type of name "article". Alternatively, if you are working with a content type that you have defined, you can code the ID directly in your application. The ID is guaranteed not to change once a content type is created.
+
+####Creating an image asset and referencing it in a content item
+Creating a new image (or other file) asset in WCH is a two-step process: first the file is uploaded to the resources service, and then the returned ID for the resource is used to create an asset. In this sample the HTML5 File object is used with XMLHttpRequest to upload the file.
+
+Once the image asset is created, the returned JSON can be used in building the element data that is used when creating a content item.
+
+####Building the elements data for the new content item
+In this example, there is a hard-coded "emptyElements" structure that matches the fields and element types that the Article content type expects. Alternatively you could use the authoring/v1/types/{id}/schema API to retrieve a Json-Schema for a content type and generate the element structure dynamically.
+
+####Using the helper functions for the steps in creating the content item
+The individual API calls used in this example are broken out into separate reusable functions so that you could use them in different ways.
+
+In this sample all of the following calls are used:
+- Login, with wchLogin
+- Upload resource, with wchCreateResource
+- Create asset from resource ID, with wchCreateAssetFromResource
+- Search for content type "article," with wchSearch
+- Create content item, with wchCreateContentItem
+
+If, for example, you just want to create a content item with only text fields using a known content type ID, you would just call:
+- wchLogin
+- wchCreateContentItem
+
+And if you just want to upload a file and create a new asset you would call:
+- wchLogin
+- wchCreateResource
+- wchCreateAssetFromResource
+
+
+###Resources
+
+API Explorer reference documentation: https://developer.ibm.com/api/view/id-618
+
+Watson Content Hub developer center: https://developer.ibm.com/wch/
+
+Watson Content Hub forum: https://developer.ibm.com/answers/smartspace/wch/
+
---
+++ public/app.js
@@ -0,0 +1,222 @@
+/*
+ * Copyright 2016  IBM Corp.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+"use strict";
+
+// Base URLs and endpoints
+const wchLoginApiGateway = "https://my.digitalexperience.ibm.com/api/";
+// Content Hub blueid username and password - replace these or add code to get these from inputs
+const username = "[username]";
+const password = "[password]";
+
+// Empty elements for Article content type
+var emptyElements = {
+    "body": {
+        "elementType": "text",
+        "value": ""
+    },
+    "title": {
+        "elementType": "text",
+        "value": ""
+    },
+    "image": {
+        "elementType": "image",
+        "renditions": {},
+        "asset": {}
+    },
+    "publishDate": {
+        "elementType": "datetime",
+        "value": ""
+    },
+    "author": {
+        "elementType": "text",
+        "value": ""
+    },
+    "category": {
+        "elementType": "category"
+    },
+    "summary": {
+        "elementType": "text",
+        "value": ""
+    }
+};
+
+const wchLoginURL = wchLoginApiGateway + "login/v1/basicauth";
+const contentService = "authoring/v1/content";
+const resourceService = "authoring/v1/resources";
+const assetService = "authoring/v1/assets";
+const searchService = "authoring/v1/search";
+
+var baseTenantUrl;
+
+// Login, upload resource, create asset, and create content item
+function createContentItem(contentTypeName, contentName, file, textData) {
+    // start with a copy of the empty elements structure for article content type
+    var elements = JSON.parse(JSON.stringify(emptyElements));
+    if (!file) {
+        return Promise.reject('No image file specified');
+    }
+    // 1. login
+    return wchLogin(username, password)
+        .then(function(baseUrl) {
+            baseTenantUrl = baseUrl; // save this for other calls
+            // 2. Upload resource and create asset
+            return wchCreateResource(file);
+        })
+        .then(function(resourceJson) {
+            var id = resourceJson.id;
+            // console.log("resource: ", resourceJson);
+            // 3. Create asset using ID from resource upload
+            return wchCreateAssetFromResource(id, file.name);
+        })
+        .then(function(assetJson) {
+            // console.log("asset: ", assetJson);
+            // set image properties in contentElements
+            var image = elements.image;
+            image.elementType = "image";
+            image.renditions = assetJson.renditions;
+            image.asset = {};
+            image.asset.id = assetJson.id;
+            image.asset.resourceUri = assetJson.renditions["default"].source;
+            image.asset.fileName = assetJson.fileName;
+            image.asset.fileSize = assetJson.fileSize;
+            image.asset.mediaType = assetJson.mediaType;
+            // 4. search for content type by name
+            var searchParams = "q=*:*&fl=name,id&wt=json&fq=classification:content-type&fq=name:" + contentTypeName;
+            return wchSearch(searchParams);
+        })
+        .then(function(searchResults) {
+            if (searchResults.numFound == 0) {
+                return Promise.reject('Content type not found: ' + contentTypeName);
+            }
+            var id = searchResults.documents[0].id;
+            var contentTypeId = id.substring(id.indexOf(":") + 1);
+            // Populate all the text fields in the elements
+            Object.keys(textData).forEach(function(key) {
+                elements[key].value = textData[key];
+            });
+            // 5. create content item
+            return wchCreateContentItem(contentName, contentTypeId, elements);
+        });
+};
+
+// Call login - callback has baseURL argument
+function wchLogin(user, pass) {
+    var requestOptions = {
+        xhrFields: {
+            withCredentials: true
+        },
+        url: wchLoginURL,
+        headers: {
+            "Authorization": "Basic " + btoa(user + ":" + pass)
+        }
+    };
+    // Call login
+    return $.ajax(requestOptions).then(function(data, textStatus, request) {
+        return request.getResponseHeader('x-ibm-dx-tenant-base-url'); // use this to get tenant from the basicauth call
+    });
+}
+
+// Upload a file to create a resource. baseTenantUrl must already be set
+function wchCreateResource(file) {
+    var createResourceUrl = baseTenantUrl + '/' + resourceService + "?name=" + file.name;
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+        xhr.open("post", createResourceUrl, true);
+        xhr.setRequestHeader("Content-Type", file.type);
+        xhr.onload = function() {
+            if (this.status >= 200 && this.status < 300) {
+                console.log('OK');
+                resolve(JSON.parse(xhr.response));
+            } else {
+                console.log('bad HTTP status');
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function() {
+            console.log('error');
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send(file);
+    });
+}
+
+// Creates an asset from a resource ID
+function wchCreateAssetFromResource(resourceId, filename) {
+    var createAssetUrl = baseTenantUrl + '/' + assetService;
+    var reqOptions = {
+        xhrFields: {
+            withCredentials: true
+        },
+        dataType: "json",
+        type: "POST",
+        data: JSON.stringify({ resource: resourceId, path: '/dxdam/' + filename, name: filename }),
+        contentType: "application/json",
+        // mediaType: mimeType,                
+        url: createAssetUrl
+    };
+    // Post to assets service
+    return $.ajax(reqOptions).done(function(json) {
+        return json;
+    });
+}
+
+// Search - callback has search results object
+function wchSearch(searchParams) {
+    // console.log('searchParams: ', searchParams);
+    var searchURL = baseTenantUrl + '/' + searchService + "?" + searchParams;
+    var reqOptions = {
+        xhrFields: {
+            withCredentials: true
+        },
+        dataType: "json",
+        url: searchURL,
+    };
+    return $.ajax(reqOptions).then(function(json) {
+        return json;
+    });
+}
+
+// create content item - callback has new content item object
+function wchCreateContentItem(name, contentTypeId, contentElements) {
+    // console.log('createContentItem baseTenantUrl: ', baseTenantUrl);
+    var createContentUrl = baseTenantUrl + '/' + contentService;
+    var data = {
+        "name": name,
+        "typeId": contentTypeId,
+        "tags": [],
+        "status": "draft",
+        "links": {},
+        "elements": contentElements
+    };
+    var reqOptions = {
+        xhrFields: {
+            withCredentials: true
+        },
+        dataType: "json",
+        contentType: "application/json",
+        type: "POST",
+        data: JSON.stringify(data),
+        url: createContentUrl
+    };
+    // console.log(JSON.stringify(reqOptions, "", 4));
+    // Post to Content service
+    return $.ajax(reqOptions).done(function(json) {
+        return json;
+    });
+
+}
